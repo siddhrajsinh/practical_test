6609c74a7ac94e302cdc61ca4f93305f42c41332
Siddhrajsinh <62494635+siddhrajsinh@users.noreply.github.com>
2025-10-30 11:25:19 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Siddhrajsinh Chauhan | Cygnet Practical</title>
+    <title>Siddhrajsinh Chauhan | Practical Test</title>
     <style>
         .collapse {
             cursor: pointer;
@@ -49,7 +49,7 @@
 
 <body>
     <main>
-        <h1>Siddhrajsinh Chauhan | Cygnet Practical</h1>
+        <h1>Siddhrajsinh Chauhan | Practical Test</h1>
         <ul>
             <li><a href="prog1.php"> Program 1 </a></li>
             <li><a href="prog2.php"> Program 2 </a></li>
