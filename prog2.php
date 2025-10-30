21a52ab83588db12eb54e0028744c862660651e3
Siddhrajsinh <62494635+siddhrajsinh@users.noreply.github.com>
2025-10-30 11:27:07 +0530

    Update prog2.php

--- prog2.php
+++ prog2.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Siddhrajsinh Chauhan | Cygnet Practical</title>
+    <title>Siddhrajsinh Chauhan | Practical Test</title>
     <style> 
         .collapse{
             cursor: pointer;
@@ -26,7 +26,7 @@
 
 <body>
     <main>
-        <h1>Siddhrajsinh Chauhan | Cygnet Practical</h1>
+        <h1>Siddhrajsinh Chauhan | Practical Test</h1>
         <ul>
             <li><a href="index.php"> Home </a></li>
             <li><a href="prog1.php"> Program 1 </a></li>
